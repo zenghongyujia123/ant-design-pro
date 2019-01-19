ed49e363de1fbabff01eaeb718ce43ed31a3d6a1
曾鸿 <>
2019-01-19 14:20:05 +0800

    Update for test

--- src/pages/Forms/JiaFangDetail.js
+++ src/pages/Forms/JiaFangDetail.js
@@ -132,13 +132,13 @@ class BasicForms extends PureComponent {
             <FormItem {...formItemLayout} label='其他3'>{getFieldDecorator('str3', { initialValue: data.str3 || '' })(<Input placeholder='请输入其他3' />)}</FormItem>
             <FormItem {...formItemLayout} label='其他4'>  {getFieldDecorator('str4', { initialValue: data.str4 || '' })(<Input placeholder='请输入其他4' />)}</FormItem>
 
-            <FormItem {...formItemLayout} label='贷超其他1'>  {getFieldDecorator('d_str1', { initialValue: data.d_str1 || '' })(<Input placeholder='请输入贷超其他1' />)}</FormItem>
-            <FormItem {...formItemLayout} label='贷超其他2'>  {getFieldDecorator('d_str2', { initialValue: data.d_str2 || '' })(<Input placeholder='请输入贷超其他2' />)}</FormItem>
-            <FormItem {...formItemLayout} label='贷超其他3'>  {getFieldDecorator('d_str3', { initialValue: data.d_str3 || '' })(<Input placeholder='请输入贷超其他3' />)}</FormItem>
-            <FormItem {...formItemLayout} label='贷超其他4'>  {getFieldDecorator('d_str4', { initialValue: data.d_str4 || '' })(<Input placeholder='请输入贷超其他4' />)}</FormItem>
-            <FormItem {...formItemLayout} label='贷超其他5'>  {getFieldDecorator('d_str5', { initialValue: data.d_str5 || '' })(<Input placeholder='请输入贷超其他5' />)}</FormItem>
-            <FormItem {...formItemLayout} label='贷超其他6'>  {getFieldDecorator('d_str6', { initialValue: data.d_str6 || '' })(<Input placeholder='请输入贷超其他6' />)}</FormItem>
-            <FormItem {...formItemLayout} label='贷超其他7'>  {getFieldDecorator('d_str7', { initialValue: data.d_str7 || '' })(<Input placeholder='请输入贷超其他7' />)}</FormItem>
+            <FormItem {...formItemLayout} label='贷超其他1'>  {getFieldDecorator('d_str1', { initialValue: data.d_str1 || '' })(<Input placeholder='2000-20000' />)}</FormItem>
+            <FormItem {...formItemLayout} label='贷超其他2'>  {getFieldDecorator('d_str2', { initialValue: data.d_str2 || '' })(<Input placeholder='日息0.01%' />)}</FormItem>
+            <FormItem {...formItemLayout} label='贷超其他3'>  {getFieldDecorator('d_str3', { initialValue: data.d_str3 || '' })(<Input placeholder='2004人申请通过' />)}</FormItem>
+            <FormItem {...formItemLayout} label='贷超其他4'>  {getFieldDecorator('d_str4', { initialValue: data.d_str4 || '' })(<Input placeholder='7-30天' />)}</FormItem>
+            <FormItem {...formItemLayout} label='贷超其他5'>  {getFieldDecorator('d_str5', { initialValue: data.d_str5 || '' })(<Input placeholder='节钱极速通道，10分钟必须下款' />)}</FormItem>
+            {/* <FormItem {...formItemLayout} label='贷超其他6'>  {getFieldDecorator('d_str6', { initialValue: data.d_str6 || '' })(<Input placeholder='请输入贷超其他6' />)}</FormItem>
+            <FormItem {...formItemLayout} label='贷超其他7'>  {getFieldDecorator('d_str7', { initialValue: data.d_str7 || '' })(<Input placeholder='请输入贷超其他7' />)}</FormItem> */}
 
             <FormItem {...submitFormLayout} style={{ marginTop: 32 }}>
               <Button type="primary" htmlType="submit" loading={submitting}>
